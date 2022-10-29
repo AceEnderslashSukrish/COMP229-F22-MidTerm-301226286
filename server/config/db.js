5070d32d55929505eb37f74678ab453ae9635807
Sukrish Gokulakrishnan <enderslash@outlook.com>
2022-10-29 19:31:31 -0400

    Configured URI to point to Atlas
    (unsuccessful)

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1/books229"
+  //"URI": "mongodb://127.0.0.1/books229"
+  "URI": "mongodb+srv://sukrish:<FjJ6oyL5aqlKClnm>@cluster0.8iermzq.mongodb.net/?retryWrites=true&w=majority"
 };
